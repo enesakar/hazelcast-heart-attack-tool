858195435cd80ace2199d80a0bd05b9095e91eac
Peter Veentjer <alarmnummer@gmail.com>
2013-07-14 20:21:14 +0300

    Java 6 compatibility

--- hazelcast-heartattack/src/main/java/com/hazelcast/heartattack/HeartAttackAlreadyThrownRuntimeException.java
+++ hazelcast-heartattack/src/main/java/com/hazelcast/heartattack/HeartAttackAlreadyThrownRuntimeException.java
@@ -1,23 +1,9 @@
 package com.hazelcast.heartattack;
 
-public class HeartAttackAlreadyThrownRuntimeException extends RuntimeException{
-
-    public HeartAttackAlreadyThrownRuntimeException() {
-    }
+public class HeartAttackAlreadyThrownRuntimeException extends RuntimeException {
 
     public HeartAttackAlreadyThrownRuntimeException(Throwable cause) {
         super(cause);
     }
 
-    public HeartAttackAlreadyThrownRuntimeException(String message) {
-        super(message);
-    }
-
-    public HeartAttackAlreadyThrownRuntimeException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public HeartAttackAlreadyThrownRuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
